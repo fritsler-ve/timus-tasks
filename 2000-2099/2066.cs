b55ca282524eca23e60b9f617524a8987f2bef28
EveryBatya <34242079+EveryBatya@users.noreply.github.com>
2019-11-08 13:34:06 +0500

    2066

---
+++ 2000-2099/2066.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Min((a - b * c), (a - b - c)));
+        }
+    }
+}
